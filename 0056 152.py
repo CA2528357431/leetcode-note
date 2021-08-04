f9b806b69169df2e86aee21cf0d408a894ae0839
CA2528357431 <2528357431@QQ.com>
2021-08-05 00:23:20 +0800

    normal

---
+++ 0056 152.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        '''
+        pos = [0]*len(nums)
+        neg = [0]*len(nums)
+        neg[0] = nums[0]
+        pos[0] = nums[0]
+        for x in range(1,len(nums)):
+            pos[x] = max(pos[x-1]*nums[x],nums[x],neg[x-1]*nums[x])
+            neg[x] = min(pos[x-1]*nums[x],nums[x],neg[x-1]*nums[x])
+        return max(pos)
+        '''
+
+        # 滚动优化
+
+        big = nums[0]
+        neg = nums[0]
+        pos = nums[0]
+        for x in range(1,len(nums)):
+            pos,neg = max(pos*nums[x],nums[x],neg*nums[x]),min(pos*nums[x],nums[x],neg*nums[x])
+            big = max(big,pos)
+        return big
+
+    # 我们需要维护 max 和 min
+    # max 来自于 min*cur 和 max*cur 和 cur
