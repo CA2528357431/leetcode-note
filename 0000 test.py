2c0eb0a8b209c035fbd21cbd397573d446aba34c
CA2528357431 <2528357431@QQ.com>
2021-09-03 15:19:36 +0800

    hard day

--- 0000 test.py
+++ 0000 test.py
@@ -1,6 +1,32 @@
-li = [3,4,2,1]
-li.sort(reverse=True)
-print(li)
+arr = [7,4,5,1,2,3,6,9,8,0,11,-3,5,9]
 
 
+def quick(l, r):
+    i = l
+    j = r
+    x = arr[l]
+    while i < j:
+        while i < j and arr[j] >= x:
+            j -= 1
+        arr[i] = arr[j]
+
+        while i < j and arr[i] <= x:
+            i += 1
+        arr[j] = arr[i]
+    arr[i] = x
+    return i
+
+
+def sort(l, r):
+    if l >= r:
+        return
+    mid = quick(l, r)
+    sort(l, mid - 1)
+    sort(mid + 1, r)
+
+
+
+sort(0,len(arr)-1)
+
+print(arr)
 
--- 0149 面试17.14.py
+++ 0149 面试17.14.py
@@ -17,11 +17,40 @@ class Solution:
         return source
         '''
 
-
+        '''
         heapq.heapify(arr)
         res = [0]*k
         for x in range(k):
             res[x] = heapq.heappop(arr)
         return res   
+        '''
 
+
+        # 快排思路
+        def quick(l, r):
+            i = l
+            j = r
+            x = arr[l]
+            while i < j:
+                while i < j and arr[j] >= x:
+                    j -= 1
+                arr[i] = arr[j]
+
+                while i < j and arr[i] <= x:
+                    i += 1
+                arr[j] = arr[i]
+            arr[i] = x
+            return i
+        def sort(l,r):
+            if l>=r:
+                return
+            mid = quick(l,r)
+            if mid>k-1:
+                sort(l,mid-1)
+            elif mid<k-1:
+                sort(mid+1,r)
+            else:
+                return
+        sort(0,len(arr)-1)
+        return arr[0:k]
 
