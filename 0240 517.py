94ac2b4256d30d7e54831e1957a38d3e22750eef
CA2528357431 <2528357431@QQ.com>
2021-09-29 23:57:08 +0800

    hard

---
+++ 0240 517.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findMinMoves(self, machines: List[int]) -> int:
+        '''
+        total = sum(machines)
+        n = len(machines)
+        if total % n != 0:
+            return -1
+        ave = total // n
+
+        times = 0
+
+        passage = [0] * n
+        for i in range(1, n):
+            passage[i] = machines[i - 1] + passage[i - 1]
+        for i in range(n):
+            tol = max(i * ave - passage[i], 0)
+            tor = max((n - i - 1) * ave - (total - passage[i] - machines[i]), 0)
+            times = max(tol + tor, times)
+        return times
+        '''
+
+        # 优化内存
+        total = sum(machines)
+        n = len(machines)
+        if total % n != 0:
+            return -1
+        ave = total // n
+
+        times = 0
+        passage = 0
+
+        for i in range(n):
+            tol = max(i * ave - passage, 0)
+            tor = max((n - i - 1) * ave - (total - passage - machines[i]), 0)
+            times = max(tol + tor, times)
+            passage += machines[i]
+
+        return times
+
+    # https://leetcode-cn.com/problems/super-washing-machines/solution/gong-shui-san-xie-noxiang-xin-ke-xue-xi-mzqia/
