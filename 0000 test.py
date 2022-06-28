284272458fead1d465fb3cd3b440fd11ce83b8bb
CA2528357431 <2528357431@QQ.com>
2022-06-28 15:48:34 +0800

    struct change

--- 0000 test.py
+++ 0000 test.py
@@ -1,6 +1,9 @@
 import os
 
-path=r'E:\leetcode'
-for i in range(700,800):
-    s = path+r"\0"+str(i)+".py"
-    open(s,"w")
+path=r'E:\leetcode\LIST4\readme.txt'
+with open(path,"w") as f:
+    for i in range(1001,1501):
+        s = f"## {i+500}.\n* \n\n"
+
+
+        f.write(s)
